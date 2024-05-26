764d81fa795dabd1d68cae24c7fb1c3116d9cd57
David <davidrecheni@gmail.com>
2024-05-26 09:56:04 +0200

    Improve UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,16 @@
 "use client";
 
-import { useWriteContract } from "wagmi";
-import abi from "@/hoarder-abi.json";
 import DepositTokens from "@/components/deposit-tokens";
+import SellCurrency from "@/components/sell-currency";
 
 export default function Index() {
-  const { data: hash, writeContract, error } = useWriteContract();
-
-  const handleSell = () => {
-    writeContract({
-      address: "0x887B16A1B66A94432C2C63eF7eCe69EeF272bDeB",
-      abi,
-      functionName: "sellCurrency",
-      args: [[["0xF3AfD1a49b0960dc1be12d7946d952fa2eFa9dB6", 10]]],
-    });
-  };
-
   return (
     <div className="flex flex-col gap-6">
       <p>Hoardr</p>
 
       <DepositTokens />
 
-      <div>
-        <button
-          onClick={handleSell}
-          className="rounded-md bg-primary text-secondary px-3 py-2"
-        >
-          Sell currency
-        </button>
-        {error && <div>Error: {error.message}</div>}
-        {hash && <div>Transaction Hash: {hash}</div>}
-      </div>
+      <SellCurrency />
     </div>
   );
 }
---
+++ components/sell-currency.tsx
@@ -0,0 +1,54 @@
+import { useAccount, useReadContract, useWriteContract } from "wagmi";
+import abi from "@/hoarder-abi.json";
+import tokenAbi from "@/hoarder-token-abi.json";
+import { useState } from "react";
+const HOARD_TKN_ADDRESS = "0xF3AfD1a49b0960dc1be12d7946d952fa2eFa9dB6";
+const HOARD_CONTRACT = "0x887B16A1B66A94432C2C63eF7eCe69EeF272bDeB";
+const SellCurrency = () => {
+  const [amount, setAmount] = useState(10000);
+  const { data: hash, writeContract, error } = useWriteContract();
+
+  const { address: connectedWalletAddress } = useAccount();
+
+  const { data: hoardBalance, error: errorHoard } = useReadContract({
+    address: HOARD_TKN_ADDRESS,
+    abi: tokenAbi,
+    functionName: "balanceOf",
+    args: [connectedWalletAddress],
+  });
+
+  const handleSell = () => {
+    writeContract({
+      address: HOARD_CONTRACT,
+      abi,
+      functionName: "sellCurrency",
+      args: [[[HOARD_TKN_ADDRESS, amount]]],
+    });
+  };
+
+  return (
+    <div className="">
+      <h1>HOARD balance: {(hoardBalance as BigInt | undefined)?.toString()}</h1>
+      <h1>
+        Amount to sell:{" "}
+        <input
+          value={amount}
+          type="number"
+          onChange={(e) => {
+            setAmount(e.target.valueAsNumber);
+          }}
+        />
+      </h1>
+      <button
+        onClick={handleSell}
+        className="rounded-md bg-primary text-secondary px-3 py-2"
+      >
+        Sell currency
+      </button>
+      {error && <div>Error: {error.message}</div>}
+      {hash && <div>Transaction Hash: {hash}</div>}
+    </div>
+  );
+};
+
+export default SellCurrency;
